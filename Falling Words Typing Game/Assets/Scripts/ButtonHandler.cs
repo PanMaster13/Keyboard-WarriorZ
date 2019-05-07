827e7a6e9decbcaa148d86a19448255006364bcf
PanMaster13 <36907215+PanMaster13@users.noreply.github.com>
2019-05-07 14:08:58 +0800

    Implemented Tutorial Menu and Story Menu
    wew2

--- Falling Words Typing Game/Assets/Scripts/ButtonHandler.cs
+++ Falling Words Typing Game/Assets/Scripts/ButtonHandler.cs
@@ -5,15 +5,24 @@ using UE = UnityEngine.SceneManagement;
 
 public class ButtonHandler : MonoBehaviour
 {
+    /*
+    * Build Index Reference (For LoadScene Function)
+    * Main Menu - 0
+    * Main - 1
+    * Tutorial - 2
+    * Story - 3
+    */
+
     public void PlayGame()
     {
         Debug.Log("Play is pressed");
-        UE.SceneManager.LoadScene(UE.SceneManager.GetActiveScene().buildIndex + 1); //Another way is EditorSceneManager.LoadScene()
+        UE.SceneManager.LoadScene(1); // Another way is UE.SceneManager.GetActiveScene().buildIndex + 1
     }
 
     public void AboutGame()
     {
         Debug.Log("About is pressed");
+        UE.SceneManager.LoadScene(2); 
     }
 
     public void ScoreBoardGame()
---
+++ Falling Words Typing Game/Assets/Scripts/StoryButtonHandler.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEditor.SceneManagement;
+
+using UE = UnityEngine.SceneManagement;
+
+public class StoryButtonHandler : MonoBehaviour
+{
+    /*
+     * Build Index Reference (For LoadScene Function)
+     * Main Menu - 0
+     * Main - 1
+     * Tutorial - 2
+     * Story - 3
+     */
+
+    public void MainMenu()
+    {
+        Debug.Log("Main Menu is pressed");
+        UE.SceneManager.LoadScene(0); //Another way is EditorSceneManager.LoadScene()
+    }
+
+    public void Tutorial()
+    {
+        Debug.Log("Tutorial is pressed");
+        UE.SceneManager.LoadScene(2);
+    }
+}
