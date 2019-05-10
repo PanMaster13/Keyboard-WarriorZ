d040f33308379a6c05f0d385d5dca6f106f6cbd6
Darren <darren.ccy17@gmail.com>
2019-05-10 19:34:18 +0800

    scoreboard

---
+++ Falling Words Typing Game/Assets/Scripts/PreviousGameScore.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using System.Text;
+public class PreviousGameScore : MonoBehaviour
+{
+    public Text text;
+    public string gameScore;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+        string filepath = Application.dataPath + "/score.txt";
+        StreamReader sr = new StreamReader(filepath);
+        string data = sr.ReadLine();
+        string data2 = sr.ReadLine();
+        gameScore = data2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = "Previous Game Score: " + gameScore;
+    }
+}
