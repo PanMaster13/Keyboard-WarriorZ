9643d327b060a8930510209523a10c6525f329be
PanMaster13 <36907215+PanMaster13@users.noreply.github.com>
2019-05-14 16:54:29 +0800

    Created endless runner mode
    wew

---
+++ Falling Words Typing Game/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    public int damage = 1;
+    public float speed;
+
+    public GameObject ooofffSound;
+
+    void Update()
+    {
+        transform.Translate(Vector2.down * speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Instantiate(ooofffSound, transform.position, Quaternion.identity);
+
+            other.GetComponent<PlayerController>().health -= damage;
+            Debug.Log(other.GetComponent<PlayerController>().health);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Falling Words Typing Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+using UE = UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    /*
+    * Build Index Reference (For LoadScene Function)
+    * Main Menu - 0
+    * Tutorial - 1
+    * Story - 2
+    * DifficultySelector - 3
+    * Game - 4
+    * EndScreen - 5
+    * ScoreBoard - 6
+    */
+
+    private Vector2 targetPos;
+    public float Xmove;
+
+    public float speed;
+    public float maxX;
+    public float minX;
+
+    public int health = 5;
+    public Text healthDisplay;
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthDisplay.text = "HP: " + health.ToString();
+
+        if (health <= 0)
+        {
+            UE.SceneManager.LoadScene(UE.SceneManager.GetActiveScene().buildIndex);
+        }
+
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.RightArrow) && transform.position.x < maxX)
+        {
+            
+            targetPos = new Vector2(transform.position.x + Xmove, transform.position.y);
+        }
+        else if(Input.GetKeyDown(KeyCode.LeftArrow) && transform.position.x > minX)
+        {
+            
+            targetPos = new Vector2(transform.position.x - Xmove, transform.position.y);
+        }
+
+        
+    }
+}
