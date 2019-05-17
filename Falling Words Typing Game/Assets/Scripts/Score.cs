733e46fa9553ecbd545755db523f5c83fb828432
Darren <darren.ccy17@gmail.com>
2019-05-17 15:37:10 +0800

    levels added

--- Falling Words Typing Game/Assets/Scripts/Score.cs
+++ Falling Words Typing Game/Assets/Scripts/Score.cs
@@ -17,12 +17,17 @@ public class Score : MonoBehaviour
     void Update()
     {
         //Debug.Log(scene.name+" "+WordManager.difficultyValue);
-        if (Score.score == 1 && scene.name.Equals("Game"))
+        if (Score.score == 30 && scene.name.Equals("Game"))
         {
             ElapsedTime.endTime = "";
             WordManager.difficultyValue = WordManager.Difficulty.Medium;
             SceneManager.LoadScene("Game 1");
         }
-
+        if (Score.score == 80 && scene.name.Equals("Game"))
+        {
+            ElapsedTime.endTime = "";
+            WordManager.difficultyValue = WordManager.Difficulty.Hard;
+            SceneManager.LoadScene("Game 2");
+        }
     }
 }
